316e992f7204197c138681916a2e914fc85fe2dc
Julien Bouyoud <jbouyoud@leadformance.com>
2018-07-25 10:05:40 +0200

    feat: allow dev namespace deployment

--- docker-nginx-entrypoint.sh
+++ docker-nginx-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 app_bundle_dir=/usr/share/nginx/html;
 
 sed -i.bak \
+    -e 's#CONFIG_KUBERNETES_API_URL#'"${CONFIG_KUBERNETES_API_URL}"'#g' \
     -e 's#CONFIG_KUBERNETES_API_KEY#'"${CONFIG_KUBERNETES_API_KEY}"'#g' \
     -e 's#CONFIG_KUBERNETES_API_SECRET#'"${CONFIG_KUBERNETES_API_SECRET}"'#g' \
     -e 's#CONFIG_KUBERNETES_WIDGET_URL#'"${CONFIG_KUBERNETES_WIDGET_URL}"'#g' \
